9eccc9b066361690077bf53375bec270120d2204
djblacker2001 <hoangvu222001@gmail.com>
2025-12-26 12:59:43 +0700

    ok

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -3,10 +3,11 @@ import Quiz from "./components/Quiz";
 function App() {
 
   return (
-      <div className="container">
-        <h1>Trắc nghiệm</h1>
-        <Quiz/>
-      </div>
+    
+    <div className="container">
+      <h1>Trắc nghiệm</h1>
+      <Quiz />
+    </div>
   )
 }
 
